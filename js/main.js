adcf0a0630b4a977b53a17452334a0dd5814477e
Vlad <vladsa000@gmail.com>
2022-11-04 11:28:15 +0200

    ready site

---
+++ js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+
+    $nav = $('.nav');
+    $toggleCollapse = $('.toggle-collapse');
+
+
+    $toggleCollapse.click(function() {
+        $nav.toggleClass('collapse')
+    });
+
+    $('.owl-carousel').owlCarousel({
+        loop: true,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsive: {
+            0: {
+                items: 1
+            },
+            320: {
+                items: 1
+            },
+            560: {
+                items: 2
+            },
+            960: {
+                items: 3
+            }
+        }
+    });
+
+    $('.move-up span').click(function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000)
+    });
+
+    AOS.init();
+
+
+});
